95f35240a8689ce1ba79d82bf41ce1ccba87056f
Madeleine <mvon@ch.ibm.com>
2015-08-05 22:04:24 +0200

    app.py added - not working

---
+++ README.md
@@ -0,0 +1,1 @@
+This simple app consists of an html file and a Flask/Python script. It takes the userinput from an html form and returns it uppercased to the browser. 
---
+++ app.py
@@ -0,0 +1,21 @@
+"""Cloud Foundry test"""
+
+from flask import Flask, render_template, request
+import os, timeit
+
+app = Flask(__name__)
+
+if os.getenv("VCAP_APP_PORT"):
+	port = int(os.getenv("VCAP_APP_PORT"))
+else:
+	port = 8080
+
+@app.route('/', methods=['GET'])
+def display_template():
+	#if "userinput" in request.args:
+	#	return request.args["userinput"].upper()
+	#else: 			
+	return render_template('index3.html')
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=port, debug=True)
